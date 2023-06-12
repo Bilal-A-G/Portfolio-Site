7c16ca0bba78b02871f41a19a596bf6cadc93387
Bilal-A-G <86686062+Bilal-A-G@users.noreply.github.com>
2023-06-12 16:18:48 -0400

    Restarted project from scratch with new design

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,34 @@
-import {BrowserRouter} from "react-router-dom";
-import Navbar from "./components/Navbar.jsx";
-import Hero from "./components/Hero.jsx";
-import Tech from "./components/Tech.jsx";
-import AboutMe from "./components/AboutMe.jsx";
-import Contact from "./components/Contact.jsx";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-const App = () => {
+function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <BrowserRouter>
-        <div className="relative bg-background h-screen w-screen overflow-x-hidden">
-            <Navbar/>
-            <Hero/>
-            <AboutMe/>
-        </div>
-        <div className="relative z-0">
-        </div>
-    </BrowserRouter>
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,jsx}"],
-  mode: "jit",
+export default {
+  content: [],
   theme: {
-    extend: {
-      colors: {
-        background: "#0c0c0c",
-        callToAction: "#ffffff",
-        minor: "#86efac",
-        selected: "#fca5a5"
-      },
-      boxShadow: {
-        card: "0px 35px 120px -15px #211e35",
-      },
-      screens: {
-        xs: "700px",
-      },
-      backgroundImage: {
-        "hero-background": "url('/src/assets/Background.jpg')",
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
+
