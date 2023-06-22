3f0643192725ff121a5f625c751a17f4427412c6
Bilal-A-G <86686062+Bilal-A-G@users.noreply.github.com>
2023-06-22 11:14:14 -0400

    Added icons for all interests in the about section, finally

--- src/components/aboutPage/Interests.jsx
+++ src/components/aboutPage/Interests.jsx
@@ -4,12 +4,26 @@
           <p className="justify-center text-white text-[16px] flex pt-[110px]">I have several </p>
           <p className="justify-center text-white text-5xl flex pt-[10px] font-semibold">Interests</p>
           <div className="w-full justify-center flex">
-              <div className="grid auto-cols-max grid-flow-col gap-5 place-items-center justify-center h-[150px] mt-[80px]">
-                  <div className="w-[140px] h-[140px] bg-[url('ChessPieces.svg')] bg-cover bg-center bg-no-repeat">
-                      <p className="text-white text-[25px] font-semibold pt-[145px] pl-[35px]">Chess</p>
+              <div className="grid auto-cols-max grid-flow-col gap-10 place-items-center justify-center h-[150px] mt-[80px]">
+                  <div className="mt-[20px]">
+                      <div className="w-[110px] h-[90px] bg-[url('Chess.svg')] bg-cover bg-center bg-no-repeat mt-[5px]"/>
+                      <p className="text-white text-[25px] font-semibold pt-[25px] w-[130px] pl-[15px]">Chess</p>
                   </div>
-                  <div className="w-[150px] h-[150px] bg-[url('Atom.svg')] bg-cover bg-center bg-no-repeat">
-                      <p className="text-white text-[25px] font-semibold pt-[150px] pl-[35px]">Science</p>
+                  <div className="mt-[20px]">
+                      <div className="w-[110px] h-[90px] bg-[url('Blender.svg')] bg-cover bg-center bg-no-repeat ml-[20px]"/>
+                      <p className="text-white text-[25px] font-semibold pt-[25px] w-[190px]">3D Modelling</p>
+                  </div>
+                  <div className="mt-[12px]">
+                      <div className="w-[110px] h-[90px] bg-[url('Programming.svg')] bg-cover bg-center bg-no-repeat ml-[20px] mt-[10px]"/>
+                      <p className="text-white text-[25px] font-semibold pt-[25px] w-[190px]">Programming</p>
+                  </div>
+                  <div className="mt-[15px]">
+                      <div className="w-[110px] h-[90px] bg-[url('Atom.svg')] bg-cover bg-center bg-no-repeat mt-[10px]"/>
+                      <p className="text-white text-[25px] font-semibold pt-[25px] w-[140px] ml-[5px]">Science</p>
+                  </div>
+                  <div className="mt-[23px]">
+                      <div className="w-[110px] h-[90px] bg-[url('Reading.svg')] bg-cover bg-center bg-no-repeat"/>
+                      <p className="text-white text-[25px] font-semibold pt-[25px] w-[50px] ml-[10px]">Reading</p>
                   </div>
               </div>
           </div>
