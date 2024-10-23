88c33813534d5549983d4641e4808030bce8cef5
Bilal <86686062+Bilal-A-G@users.noreply.github.com>
2024-10-23 12:59:27 -0400

    Made website fully responsive, couldn't think of a better design for mobile for the timeline, so it's just a list for now

--- src/components/homePage/ProfessionalExperience.jsx
+++ src/components/homePage/ProfessionalExperience.jsx
@@ -13,18 +13,18 @@ const ProfessionalExperience = () => {
             <FadeIn>
                 <p className={`w-full justify-center mt-[1%] flex ${Styles.SectionSubHeader}`}>Professional Background</p>
             </FadeIn>
-            <div className="w-[3px] xl:h-[960px] lg:h-[915px] md:h-[783px] absolute left-1/2 mt-[4%] bg-white"/>
+            <div className=" md:flex hidden w-[3px] xl:h-[960px] lg:h-[915px] md:h-[783px] absolute left-1/2 mt-[4%] bg-white"/>
             <div className="w-full lg:gap-[6%] lg:px-[6%] md:gap-y-[6%] sm:gap-y-[2%] gap-y-[2%] mt-[8%] justify-center grid place-items-center md:grid-cols-1 lg:grid-cols-1 auto-rows-max md:grid-flow-cols">
                 {ProfessionalExperienceList.map(({name, longDescription, date, position}, i) =>
                     <div>
-                        <div className={`w-[30px] h-[30px] border-white border-2 absolute lg:ml-[362px] md:ml-[312px] rounded-2xl`}>
+                        <div className={`md:flex hidden w-[30px] h-[30px] border-white border-2 absolute ${i % 2 === 0 ? "lg:ml-[362px] md:ml-[312px]" : "lg:ml-[370px] md:ml-[317px]"} rounded-2xl`}>
                             <div className={`w-full h-full place-items-center justify-center flex`}>
                                 <div className={`w-[10px] h-[10px] bg-white rounded-2xl`}></div>
                             </div>
                         </div>
-                        <div className={`${i % 2 === 0 ? "lg:pr-[400px] md:pr-[350px]" : "lg:pl-[400px] md:pl-[350px]"}`}>
+                        <div className={`${i % 2 === 0 ? "lg:pr-[400px] md:pr-[350px] sm:pr-[0px]" : "lg:pl-[417px] md:pl-[360px] sm:pl-[0px]"}`}>
                             <FadeIn>
-                                <div className="text-white lg:w-[350px] md:w-[300px] border-2 rounded-lg">
+                                <div className="text-white lg:w-[350px] md:w-[300px] sm:w-[320px] w-[270px] border-2 rounded-lg">
                                     <p className={`text-white ${Styles.SubSectionHeader} mt-[10%] justify-center text-center flex font-extrabold`}>{name}</p>
                                     <p className={`text-white ${Styles.Body} mt-[2%] justify-center text-center flex`}>{position}</p>
                                     <p className={`text-white ${Styles.Subtitle} mt-[2%] justify-center text-center flex italic`}>{date}</p>
